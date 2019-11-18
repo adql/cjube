620a2310f2980848b31adfdc8f2987d3b3da0f62
Amir Dekel <6893125+adql@users.noreply.github.com>
2019-11-18 16:54:44 +0100

    Completed the entire rotation scheme; renaming

--- src/cube/core.clj
+++ src/cube/core.clj
@@ -1,4 +1,5 @@
-(ns cube.core)
+(ns cube.core
+  (:require [clojure.string :as s]))
 
 (def solved-cube
   [[1 1 1 1 1 1 1 1 1]                  ; left
@@ -9,10 +10,10 @@
    [6 6 6 6 6 6 6 6 6]                  ; up
    ])
 
-;; Four fundamental rotation schemes on which all other are based. Defined rather brute here, maybe
+;; Four fundamental rotation schemes on which all other are based. Defined rather brutely here, maybe
 ;; I'll make it nicer when I understand better how
 
-(defn- rotate-front
+(defn- permute-front
   "Rotate the front face clockwise once"
   [cube]
   (let [[[l1 l2 l3 l4 l5 l6 l7 l8 l9]
@@ -28,7 +29,7 @@
      [r7 r4 r1 d4 d5 d6 d7 d8 d9]
      [u1 u2 u3 u4 u5 u6 l9 l6 l3]]))
 
-(defn- rotate-x
+(defn- permute-x
   "Rotates the entire cube upwards around the x-axis once"
   [cube]
   (let [[[l1 l2 l3 l4 l5 l6 l7 l8 l9]
@@ -45,7 +46,7 @@
      [f1 f2 f3 f4 f5 f6 f7 f8 f9]
      ]))
 
-(defn- rotate-y
+(defn- permute-y
   "Rotates the entire cube rightwards around the y-axis once"
   [cube]
   (let [[[l1 l2 l3 l4 l5 l6 l7 l8 l9]
@@ -61,7 +62,7 @@
      [d3 d6 d9 d2 d5 d8 d1 d4 d7]
      [u7 u4 u1 u8 u5 u2 u9 u6 u3]]))
 
-(defn- rotate-z
+(defn- permute-z
   "Rotates the entire cube rightwards around the z-axis once"
   [cube]
   (let [[[l1 l2 l3 l4 l5 l6 l7 l8 l9]
@@ -78,27 +79,50 @@
      [l7 l4 l1 l8 l5 l2 l9 l6 l3]]))
 
 (def basic-rotations
-  {:L [rotate-y rotate-y rotate-y rotate-front rotate-y]
-   :R [rotate-y rotate-front rotate-y rotate-y rotate-y]
-   :F [rotate-front]
-   :B [rotate-y rotate-y rotate-front rotate-y rotate-y]
-   :D [rotate-x rotate-front rotate-x rotate-x rotate-x]
-   :U [rotate-x rotate-x rotate-x rotate-front rotate-x]})
+  "Generates the map for all basic rotations (to be called by rotate-single)"
+  (let [basic {:F [permute-front], :X [permute-x], :Y [permute-y], :Z [permute-z]}]
+    (reduce (fn [current-map rotation]
+              (assoc current-map (first rotation) (reduce #(into %1 (%2 current-map)) [] (second rotation))))
+            basic
+            [[:L [:Y :Y :Y :F :Y]]
+             [:R [:Y :F :Y :Y :Y]]
+             [:B [:Y :Y :F :Y :Y]]
+             [:D [:X :F :X :X :X]]
+             [:U [:X :X :X :F :X]]
+             [:l [:R :X :X :X]]
+             [:r [:L :X]]
+             [:f [:B :Z]]
+             [:b [:F :Z :Z :Z]]
+             [:d [:U :Y :Y :Y]]
+             [:u [:D :Y]]
+             [:M [:l :L :L :L]]
+             [:E [:d :D :D :D]]
+             [:S [:f :F :F :F]]])))
 
-(defn- rotate-once [cube face]
+(defn- rotate-single [cube face]
+  "Perform a single movement of a face (that is, simple R, L, F, etc.)"
   ((apply comp (reverse (face basic-rotations))) cube))
 
 (defn- get-face [rotation]
+  "Extracts the face from a string of standard notation"
   (-> rotation first str keyword))
 
 (defn- get-iteration [rotation]
+  "Extracts the iteration number from a string of standard notation"
   (if (second rotation)
     (get {\2 2 \' 3} (second rotation))
     1))
 
-(defn rotate
+(defn- rotate-full
+  "Perform one move of full rotation (R, L', U2, etc.)"
   [cube rotation]
   (let [face (get-face rotation)]
     (loop [cube cube
            i (get-iteration rotation)]
-      (if (= i 1) (rotate-once cube face) (recur (rotate-once cube face) (dec i))))))
+      (if (= i 1) (rotate-single cube face) (recur (rotate-once cube face) (dec i))))))
+
+(defn rotate
+  "Perform a series of movements given as a string"
+  [cube rotations]
+  (let [rotations (s/split rotations #" ")]
+    (reduce rotate-full cube rotations)))
